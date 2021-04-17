af39727a6134e9beeda2f5a3a96b226c1fbc330c
Daoud <99m.daoud@gmail.com>
2021-04-17 13:15:14 +0200

    printed dynamic meals list

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 import MealItemForm from './MealItemForm';
 
-import classes from './MealItem.module.css'
-const MealItem = () => {
+import classes from './MealItem.module.css';
+
+const MealItem = (props) => {
     return (
         <li className={classes.meal}>
             <div>
-                <h3>Nome piatto</h3>
-                <div className={classes.description}>Descrizione dettagliata del piatto</div>
-                <div className={classes.price}>prezzo €</div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{props.price} €</div>
             </div>
             <div>
                 <MealItemForm />
